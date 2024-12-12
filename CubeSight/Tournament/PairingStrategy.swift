57146cc1d3a5150fab4b3e6d2bc4ad0bf099f903
Noe Thalheim <noe@thalheim.email>
2024-12-12 09:25:09 +0100

    Prevent silent error when player has no performance

--- CubeSight/Tournament/PairingStrategy.swift
+++ CubeSight/Tournament/PairingStrategy.swift
@@ -18,10 +18,12 @@ struct SwissPairingStrategy: PairingStrategy {
     func hasPlayed(_ player1: Player, _ player2: Player) -> Bool {
       return performance[player1]?.opponents.contains(player2) ?? false
     }
-
-    let unpairedPlayers = players.sorted {
-      (performance[$0]?.matchPoints ?? 0) > (performance[$1]?.matchPoints ?? 0)
+    
+    guard players.allSatisfy({ performance[$0] != nil }) else {
+      fatalError("all player need to have performance")
     }
+            
+    let unpairedPlayers = players.sorted { performance[$0]!.matchPoints > performance[$1]!.matchPoints }
 
     var matches: [Match] = []
     var remainingPlayers = unpairedPlayers
