e46dacf884b35d544fd3cb5cd40cffb6ca330021
Yannik Daellenbach <yannik@daellenbach.org>
2024-12-26 21:48:36 +0100

    Init `sampleData` PreviewModifier

---
+++ CubeSight/Preview/SampleData.swift
@@ -0,0 +1,21 @@
+import SwiftData
+import SwiftUI
+
+struct SampleData: PreviewModifier {
+
+  static func makeSharedContext() async throws -> ModelContainer {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try ModelContainer(for: Cube.self, Card.self, configurations: config)
+    Cube.makeSampleCube(in: container)
+    return container
+  }
+
+  func body(content: Content, context: ModelContainer) -> some View {
+    content.modelContainer(context)
+  }
+}
+
+extension PreviewTrait where T == Preview.ViewTraits {
+  @available(iOS 18.0, *)
+  @MainActor static var sampleData: Self = .modifier(SampleData())
+}
