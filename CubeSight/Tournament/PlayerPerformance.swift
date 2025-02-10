87d26fa29ab4a26da264f888c540934eb5468b22
Yannik Daellenbach <yannik@daellenbach.org>
2025-02-10 21:26:24 +0100

    Init tournament

---
+++ CubeSight/Tournament/PlayerPerformance.swift
@@ -0,0 +1,67 @@
+struct PlayerPerformance {
+  static let miniumPercentage: Double = 0.33
+
+  var matchWins: Int = 0
+  var matchLosses: Int = 0
+  var gameWins: Int = 0
+  var gameLosses: Int = 0
+  var draws: Int = 0
+  //  TODO(bye): add variable
+
+  // TODO: is this only storing the PlayerID?
+  var opponents: [Player] = []
+
+  init() {}
+
+  var matchPoints: Int {
+    return matchWins * 3 + draws
+  }
+
+  var gamePoints: Int {
+    return gameWins * 3 + draws
+  }
+
+  var totalMatches: Int {
+    return matchWins + matchLosses + draws
+  }
+
+  var totalGames: Int {
+    return gameWins + gameLosses + draws
+  }
+
+  var matchWinRate: Double {
+    let tmp = Double(matchPoints) / Double(totalMatches * 3)
+    if tmp.isFinite && tmp > PlayerPerformance.miniumPercentage {
+      return tmp
+    } else {
+      return PlayerPerformance.miniumPercentage
+    }
+  }
+
+  var gameWinRate: Double {
+    let tmp = Double(gamePoints) / Double(totalGames * 3)
+    if tmp.isFinite && tmp > PlayerPerformance.miniumPercentage {
+      return tmp
+    } else {
+      return PlayerPerformance.miniumPercentage
+    }
+  }
+
+  func matchWinRate(for opponents: [PlayerPerformance]) -> Double {
+    return opponents.reduce(into: 0.0) { $0 += $1.matchWinRate } / Double(opponents.count)
+  }
+
+  func gameWinRate(for opponents: [PlayerPerformance]) -> Double {
+    return opponents.reduce(into: 0.0) { $0 += $1.gameWinRate } / Double(opponents.count)
+  }
+
+  func matchWinRate(for opponentsMap: [Player: PlayerPerformance]) -> Double {
+    return opponents.reduce(into: 0.0) { $0 += opponentsMap[$1]?.matchWinRate ?? 0.0 }
+      / Double(opponentsMap.count)
+  }
+
+  func gameWinRate(for opponentsMap: [Player: PlayerPerformance]) -> Double {
+    return opponents.reduce(into: 0.0) { $0 += opponentsMap[$1]?.gameWinRate ?? 0.0 }
+      / Double(opponentsMap.count)
+  }
+}
