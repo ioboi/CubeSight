f3900c4125c4c95ddcfb22ac855a7b04c220ee22
Yannik Daellenbach <yannik@daellenbach.org>
2025-02-10 21:26:24 +0100

    Init tournament

---
+++ CubeSight/Data/Tournament.swift
@@ -0,0 +1,67 @@
+import Foundation
+import SwiftData
+
+@Model
+class Tournament {
+  @Relationship(deleteRule: .cascade) var rounds: [Round]
+  @Relationship var players: [Player]
+  var currentRoundIndex: Int
+  var createdAt: Date
+
+  @Transient
+  private var _performance: [Player: PlayerPerformance]?
+
+  var performance: [Player: PlayerPerformance] {
+    if let cached = _performance {
+      return cached
+    }
+    let calculated = calculatePerformance()
+    _performance = calculated
+    return calculated
+  }
+
+  func getPerformance(for player: Player) -> PlayerPerformance? {
+    performance[player]
+  }
+
+  private func calculatePerformance() -> [Player: PlayerPerformance] {
+    let matches: [Match] = rounds.flatMap { $0.matches }.filter { $0.isComplete() }
+    var performance = Dictionary(uniqueKeysWithValues: players.map { ($0, PlayerPerformance()) })
+    matches.forEach { $0.process(into: &performance) }
+    return performance
+  }
+
+  func invalidatePerformanceCache() {
+    _performance = nil
+  }
+
+  func startNextRound(strategy: PairingStrategy) {
+    //  TODO(performance): only add current round peformance, i.e. update tournament.performance instead
+    invalidatePerformanceCache()
+    //  TODO: add guard that previous round is complete
+
+    let newMatches = strategy.createPairings(for: players, with: performance)
+    let newRound = Round(matches: newMatches)
+
+    rounds.append(newRound)
+  }
+
+  init(players: [Player]) {
+    self.rounds = []
+    self.currentRoundIndex = 0
+    self.players = players
+    self.createdAt = Date.now
+  }
+}
+
+@Model
+class Round {
+  var matches: [Match]
+  // TODO: make this a round state enum (running, done)
+  var isCompleted: Bool
+
+  init(matches: [Match]) {
+    self.matches = matches
+    self.isCompleted = false
+  }
+}
