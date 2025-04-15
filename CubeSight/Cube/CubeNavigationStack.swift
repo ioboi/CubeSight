88447cd6b04ae8099ac164f5108fce14f5ec88cd
Yannik Daellenbach <yannik@daellenbach.org>
2025-04-15 20:00:02 +0200

    Use `NavigationSplitView` and implement modern navigation API

--- CubeSight/Cube/CubeNavigationStack.swift
+++ CubeSight/Cube/CubeNavigationStack.swift
@@ -2,30 +2,28 @@ import SwiftData
 import SwiftUI
 
 struct CubeNavigationStack: View {
-
   @Query var cubes: [Cube]
   @State private var importing = false
-
+  @State private var selection: Cube? = nil
+  
+  @ViewBuilder
+  var contentUnavailableView: some View {
+    ContentUnavailableView {
+      Text("No Cubes 😭")
+    } description: {
+      Text("Import cubes from Cube Cobra.")
+    } actions: {
+      Button(action: { importing = true }) {
+        Label("Import \"Vintage Cube Season 4\"", systemImage: "square.and.arrow.down")
+      }
+    }
+  }
+  
   var body: some View {
-    NavigationStack {
-      List {
-        NavigationLink(destination: TextRecognitionView()) {
-          Label("Card Text Recognition", systemImage: "text.viewfinder")
-        }
+    NavigationSplitView {
+      List(selection: $selection) {
         ForEach(cubes) { cube in
-          NavigationLink(cube.name, destination: CubeDetailView(cube: cube).navigationTitle(cube.name))
-        }
-      }.overlay {
-        if cubes.isEmpty {
-          ContentUnavailableView {
-            Text("No Cubes 😭")
-          } description: {
-            Text("Import cubes from Cube Cobra.")
-          } actions: {
-            Button(action: { importing = true }) {
-              Label("Import \"Vintage Cube Season 4\"", systemImage: "square.and.arrow.down")
-            }
-          }
+          NavigationLink(cube.name, value: cube)
         }
       }
       .sheet(isPresented: $importing) {
@@ -33,10 +31,25 @@ struct CubeNavigationStack: View {
           ImportCubeView(shortId: "dimlas4")
         }.interactiveDismissDisabled()
       }
+      .overlay {
+        if cubes.isEmpty {
+          contentUnavailableView
+        }
+      }
       .navigationTitle("Cubes")
+    } detail: {
+      if let cube = selection {
+        NavigationStack {
+          CubeDetailView(cube: cube)
+            .navigationTitle(cube.name)
+        }
+      } else {
+        if !cubes.isEmpty {
+          Text("Select a cube")
+        }
+      }
     }
   }
-
 }
 
 #Preview(traits: .sampleData) {
