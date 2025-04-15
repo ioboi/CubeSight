c02bfe67370eab4adee6ab3b1fef2e183f035fe5
Yannik Daellenbach <yannik@daellenbach.org>
2025-04-15 20:00:02 +0200

    Use `NavigationSplitView` and implement modern navigation API

--- CubeSight/Cube/CubeDetailView.swift
+++ CubeSight/Cube/CubeDetailView.swift
@@ -1,6 +1,33 @@
 import SwiftData
 import SwiftUI
 
+enum CubeDetail: Codable, Hashable, Identifiable {
+  case allCards
+  
+  var id: CubeDetail { self }
+  
+  var name: String {
+    switch self {
+    case .allCards:
+      String(localized: "All", comment: "CubeDetail show all cards")
+    }
+  }
+  
+  @ViewBuilder
+  var view: some View {
+    NavigationLink(self.name, value: self)
+  }
+  
+  @ViewBuilder
+  func destination(with cube: Cube) -> some View {
+    switch self {
+    case .allCards:
+      CardList(cube: cube)
+        .navigationTitle("All Cards")
+    }
+  }
+}
+
 struct CubeDetailView: View {
   private let cube: Cube
   
@@ -8,28 +35,29 @@ struct CubeDetailView: View {
     self.cube = cube
   }
   
+  @ViewBuilder
+  var cubeBanner: some View {
+    if let imageData = cube.image {
+      Image(uiImage: UIImage(data: imageData)!)
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .listRowInsets(EdgeInsets())
+    }
+  }
+  
   var body: some View {
     List {
-      if let imageData = cube.image {
-        Section {
-          Image(uiImage: UIImage(data: imageData)!)
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .listRowInsets(EdgeInsets())
-        }
-      }
+      cubeBanner
       Section("Cards") {
-        NavigationLink(
-          destination: CardList(cube: cube)
-            .navigationTitle("All")
-        ) {
-          Text("All")
-            .badge(cube.mainboard.count)
-        }
+        CubeDetail.allCards.view
       }
+    }
+    .navigationDestination(for: CubeDetail.self) { detail in
+      detail.destination(with: cube)
     }
   }
 }
+
 
 #Preview(traits: .sampleData) {
   CubeDetailView(cube: Cube.sampleCube)
