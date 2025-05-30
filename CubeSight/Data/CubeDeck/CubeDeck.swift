49844df01210e94a54a75b26a0584633ce3d19e6
Yannik Daellenbach <git@daellenbach.org>
2025-05-30 14:50:20 +0200

    Move `CubeDeckCard` to new file

--- CubeSight/Data/CubeDeck/CubeDeck.swift
+++ CubeSight/Data/CubeDeck/CubeDeck.swift
@@ -1,29 +1,14 @@
 import Foundation
 import SwiftData
 
-@Model class CubeDeckCard {
-  
-  @Relationship(deleteRule: .cascade, inverse: \CubeDeck.cards) var deck: CubeDeck?
-  
-  var card: Card
-  var quantity: Int
-  
-  init(deck: CubeDeck, card: Card, quantity: Int = 1) {
-    self.deck = deck
-    self.card = card
-    self.quantity = quantity
-  }
-}
-
 @Model class CubeDeck {
   var id: UUID
   @Attribute var createdAt: Date
   @Attribute var name: String
-  
+
   var cube: Cube
-  
   var cards: [CubeDeckCard] = []
-  
+
   init(cube: Cube, name: String = "", createdAt: Date = Date()) {
     self.id = UUID()
     self.cube = cube
@@ -35,13 +20,13 @@ import SwiftData
 extension CubeDeck {
   @MainActor static var previewCubeDecks: [CubeDeck] = [
     CubeDeck(cube: Cube.sampleCube, name: "Sample Aggro Deck"),
-    CubeDeck(cube: Cube.sampleCube)
+    CubeDeck(cube: Cube.sampleCube),
   ]
-  
+
   @MainActor static var previewCubeDeckCards: [CubeDeckCard] = [
     CubeDeckCard(deck: previewCubeDecks[0], card: Card.blackLotus)
   ]
-  
+
   @MainActor static func makeSampleCubeDecks(in context: ModelContainer) {
     for cubeDeck in previewCubeDecks {
       context.mainContext.insert(cubeDeck)
