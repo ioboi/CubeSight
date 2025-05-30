aee36433ecd08c8592fb64d0d4cc7b2efbc49e1f
Yannik Daellenbach <git@daellenbach.org>
2025-05-30 20:46:10 +0200

    Remove progress tracking on round

--- CubeSight/Data/Tournament/TournamentRound.swift
+++ CubeSight/Data/Tournament/TournamentRound.swift
@@ -4,14 +4,11 @@ import SwiftData
 class TournamentRound {
   var tournament: Tournament
   var matches: [TournamentMatch]
-  // TODO: make this a round state enum (running, done)
-  var isCompleted: Bool
   var roundIndex: Int
 
   init(tournament: Tournament, matches: [TournamentMatch], roundIndex: Int) {
     self.tournament = tournament
     self.matches = matches
-    self.isCompleted = false
     self.roundIndex = roundIndex
   }
 }
