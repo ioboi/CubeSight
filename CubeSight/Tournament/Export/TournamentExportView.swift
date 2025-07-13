2f2f1956c2aaef2864f9597dd84a4f77e13eff26
Yannik Daellenbach <git@daellenbach.org>
2025-07-13 17:39:02 +0200

    First export

---
+++ CubeSight/Tournament/Export/TournamentExportView.swift
@@ -0,0 +1,150 @@
+import SwiftData
+import SwiftUI
+import TabularData
+import Zip
+
+struct TournamentExportView: View {
+  let tournament: Tournament
+
+  @State private var isShareSheetPresented: Bool = false
+
+  private enum ExportState {
+    case exporting
+    case done(URL)
+    case error(Error)
+  }
+
+  @State private var exportState: ExportState = .exporting
+
+  var body: some View {
+    switch exportState {
+    case .exporting:
+      ProgressView("Preparing export")
+        .task {
+          await export()
+        }
+    case .done(let url):
+      VStack {
+        Text(url.lastPathComponent)
+        ShareLink("Share File", item: url)
+          .buttonStyle(.borderedProminent)
+          .controlSize(.large)
+      }
+    // TODO: Share Button
+    case .error(let error):
+      Text("Error: \(error)")  //TODO: Make it better!
+    }
+  }
+
+  private func export() async {
+    let documentsDirectory = FileManager.default.temporaryDirectory
+
+    do {
+      struct MatchExport: Codable {
+        let tournamentDate: Date
+        let round: Int
+        let player1: String
+        let player2: String
+        let player1Wins: Int
+        let player2Wins: Int
+        let draws: Int
+      }
+
+      let matches = tournament.rounds
+        .map { ($0.roundIndex, $0.matches) }
+        .flatMap { roundIndex, matches in
+          matches.map { match in
+            MatchExport(
+              tournamentDate: Calendar.current.startOfDay(
+                for: tournament.createdAt
+              ),
+              round: roundIndex + 1,
+              player1: match.player1.name,
+              player2: match.player2.name,
+              player1Wins: match.player1Wins,
+              player2Wins: match.player2Wins,
+              draws: match.draws
+            )
+          }
+        }
+
+      let encoder = JSONEncoder()
+      encoder.dateEncodingStrategy = .iso8601
+
+      let tournamentData = try encoder.encode(matches)
+      let tournamentDataFrame = try DataFrame(jsonData: tournamentData)
+
+      let formatter = {
+        let result = DateFormatter()
+        result.dateFormat = "yyyy_MM_dd"
+        return result
+      }()
+
+      let tournamentExportName =
+        "\(formatter.string(from: tournament.createdAt))_matches.csv"
+
+      let fileURL = documentsDirectory.appendingPathComponent(
+        tournamentExportName
+      )
+      try tournamentDataFrame.writeCSV(to: fileURL)
+
+      struct CardExport: Codable {
+        let tournament: Date
+        let player: String
+        let scryfallId: String
+        let quantity: Int
+      }
+
+      let tournamentDate = Calendar.current.startOfDay(
+        for: tournament.createdAt
+      )
+
+      let tournamentDraftedDecks = tournament.players
+        .compactMap { $0.draftedDeck }
+        .flatMap { draftedDeck in
+          draftedDeck.cards.map { card in
+            CardExport(
+              tournament: tournamentDate,
+              player: draftedDeck.tournamentPlayer!.name,
+              scryfallId: card.card.scryfallId,
+              quantity: card.quantity
+            )
+          }
+        }
+
+      let draftedDeckData = try encoder.encode(tournamentDraftedDecks)
+      let draftedDeckDataFrame = try DataFrame(jsonData: draftedDeckData)
+
+      let draftedDeckExportName =
+        "\(formatter.string(from: tournament.createdAt))_drafted_decks.csv"
+
+      let draftedDeckFileURL = documentsDirectory.appendingPathComponent(
+        draftedDeckExportName
+      )
+      try draftedDeckDataFrame.writeCSV(to: draftedDeckFileURL)
+
+      let zipFilePath = documentsDirectory.appendingPathComponent(
+        "\(formatter.string(from: tournament.createdAt))_tournament_export.zip"
+      )
+      try Zip.zipFiles(
+        paths: [fileURL, draftedDeckFileURL],
+        zipFilePath: zipFilePath,
+        password: nil,
+        progress: nil
+      )
+
+      // Cleanup csvs
+      try FileManager.default.removeItem(at: fileURL)
+
+      // TODO: Clean up zips
+
+      exportState = .done(zipFilePath)
+    } catch {
+      self.exportState = .error(error)
+    }
+  }
+}
+
+#Preview(traits: .sampleData) {
+  TournamentExportView(tournament: Tournament.previewTournament)
+}
--- CubeSight/Tournament/TournamentDetail/EndedTournamentView.swift
+++ CubeSight/Tournament/TournamentDetail/EndedTournamentView.swift
@@ -3,10 +3,16 @@ import SwiftUI
 struct EndedTournamentView: View {
   let tournament: Tournament
 
+  @State private var isExportTournamentPresented = false
+
   var body: some View {
     List {
       NavigationLink("Standings") {
         StandingsView(tournament: tournament)
+      }
+      
+      Section {
+        Button("Export Tournament", systemImage: "square.and.arrow.up") { isExportTournamentPresented = true }
       }
     }
     .toolbar {
@@ -15,6 +21,12 @@ struct EndedTournamentView: View {
       }
       ToolbarItem(placement: .primaryAction) {
         Button("Reopen", action: reopen)
+      }
+    }
+    .sheet(isPresented: $isExportTournamentPresented) {
+      NavigationStack {
+        TournamentExportView(tournament: tournament)
+          .navigationTitle("Export Tournament")
       }
     }
   }
